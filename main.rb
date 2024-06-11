cf3ec4092fc74ea94e5b433e2ce4238d8a707b08
Namrone <151798569+Namrone@users.noreply.github.com>
2024-06-10 20:58:30 -0700

    Added some comments to the code

--- main.rb
+++ main.rb
@@ -6,10 +6,10 @@ user_string = gets.chomp
 def substrings(words, sub_string_list) #=> find matches of each word from user_string to the dictionary
     word_count = Hash.new
     arr_words = []
-    arr_words = words.split(/\W+/)
+    arr_words = words.split(/\W+/) #=> Removes punctuations and puts each word into an array
     arr_words = arr_words.each do |word|
         down_word = word.downcase
-        if sub_string_list.include? (down_word)
+        if sub_string_list.include? (down_word) #=> Adds the word and increments the count if a word is located in the substring list
             if word_count.has_key?(down_word)
                 word_count[down_word] += 1
             else
