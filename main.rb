32624efae4edf5a6e4ddd99c1e65d2b26c4359f2
Namrone <151798569+Namrone@users.noreply.github.com>
2024-06-10 20:56:45 -0700

    Added all logic and substring method to return a hash with counts of strings in the substring

---
+++ main.rb
@@ -0,0 +1,23 @@
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+p "Please enter a string."
+user_string = gets.chomp
+
+def substrings(words, sub_string_list) #=> find matches of each word from user_string to the dictionary
+    word_count = Hash.new
+    arr_words = []
+    arr_words = words.split(/\W+/)
+    arr_words = arr_words.each do |word|
+        down_word = word.downcase
+        if sub_string_list.include? (down_word)
+            if word_count.has_key?(down_word)
+                word_count[down_word] += 1
+            else
+                word_count[down_word] = 1
+            end
+        end
+    end
+    p word_count
+end
+
+substrings(user_string, dictionary)
